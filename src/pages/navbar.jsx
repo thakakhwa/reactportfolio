61e2edcf444aa0b48859b700ade2d920fdf3614d
thakakhwa <zaidqal@hotmail.com>
2025-04-05 14:05:52 +0300

    mergin css files

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { FaLinkedin, FaGithub, FaMoon, FaSun } from "react-icons/fa";
-import "./navbar.css";
 import "./index.css"
 
 const Navbar = () => {
